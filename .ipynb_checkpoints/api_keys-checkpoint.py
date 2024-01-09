1c516d310fb51b607da9cb7836448347c8e547bc
paoloarciaga <paoloarciaga@Paolos-MacBook-Pro.local>
2024-01-08 16:10:38 -0800

    Pushing files to GitHub

---
+++ .ipynb_checkpoints/api_keys-checkpoint.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "5f7b62ed31a9c3c9ca2d3be27715f343"
+
+# Geoapify API Key
+geoapify_key = "88e80e5c306344fdb8fbb01fe022c051"
---
+++ .ipynb_checkpoints/api_keys_example-checkpoint.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "ENTER OPEN WEATHERKEY HERE"
+
+# Geoapify API Key
+geoapify_key = "ENTER GEOAPIFY KEY HERE"
